f9d037fc2f277b13ef3c70ca4d61f272a505fae9
Garreth <Garreth>
2022-04-20 15:44:09 -0500

    first push

---
+++ tetris/src/application/Tetris.java
@@ -0,0 +1,23 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+public class Tetris {
+
+    @FXML
+    private TextArea lineCounter;
+
+    @FXML
+    private TextArea timeCounter;
+
+    @FXML
+    private TextArea scoreCounter;
+
+    @FXML
+    void ff4444(ActionEvent event) {
+
+    }
+
+}
