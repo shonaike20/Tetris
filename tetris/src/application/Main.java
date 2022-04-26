59de26a379c4193501cd2226500128a0f5c4c66b
Garreth <Garreth>
2022-04-26 00:19:04 -0500

    Just an update for the repo, no significant changes

--- tetris/src/application/Main.java
+++ tetris/src/application/Main.java
@@ -14,14 +14,15 @@ import javafx.scene.media.MediaPlayer;
 
 public class Main extends Application {
 	@Override
+	
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Tetris.fxml"));
 			Scene scene = new Scene(root,400,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			 String path = "src/application/tetrismenu.mp3";  
+			 String path = "src/application/tetrisgame.mp3";  
 	          
 		        //Instantiating Media class  
 		        Media media = new Media(new File(path).toURI().toString());  
--- tetris/src/application/MainController.java
+++ tetris/src/application/MainController.java
@@ -1,5 +1,29 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.Stage;
+
 public class MainController {
 	
+	private void play_audio() {
+		String musicFile = "tetrismenu.mp3";
+		Media sound = new Media(new File(musicFile).toURI().toString());
+		MediaPlayer mediaPlayer = new MediaPlayer(sound);
+		mediaPlayer.play();
+		
+		
+	}
 }
